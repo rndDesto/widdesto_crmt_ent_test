13c0814f8a1a3129f1479e5c199b11e249d9a657
widdesto.yudistiro <widdesto.yudistiro@metraplasa.co.id>
2020-09-08 14:47:52 +0700

    deploy gh

---
+++ build/precache-manifest.2905c93a44fdc324a2d489c383c0a902.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c9c94ccdef408dc1e317f78d4343e3",
+    "url": "/widdesto_crmt_ent_test/index.html"
+  },
+  {
+    "revision": "51c2c4f7e15577476324",
+    "url": "/widdesto_crmt_ent_test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9e3930aca9f14bb0d5d9",
+    "url": "/widdesto_crmt_ent_test/static/js/2.263580fb.chunk.js"
+  },
+  {
+    "revision": "8a9a2ae4a25f202f52818f8f8074424e",
+    "url": "/widdesto_crmt_ent_test/static/js/2.263580fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51c2c4f7e15577476324",
+    "url": "/widdesto_crmt_ent_test/static/js/main.eaea21e9.chunk.js"
+  },
+  {
+    "revision": "a6ab6c99b65c54de0426",
+    "url": "/widdesto_crmt_ent_test/static/js/runtime-main.3b7128be.js"
+  },
+  {
+    "revision": "a5a74b4ff9be5b81b4ee10c0f456d8e9",
+    "url": "/widdesto_crmt_ent_test/static/media/work-desk__dustin-lee.a5a74b4f.jpg"
+  }
+]);
